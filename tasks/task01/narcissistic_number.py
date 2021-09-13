3d26bcdb94ccc740ed09afb44a87a833b7dc6135
myepishkin <myepishkin@exadel.com>
2021-09-13 12:40:04 +0300

    task01. Narcissistic numbers. Fixed manipulations with str.

--- tasks/task01/narcissistic_number.py
+++ tasks/task01/narcissistic_number.py
@@ -1,34 +1,40 @@
 k = int(input('Please enter an integer k(1<=k<=1000): '))
-if 1 <= k <= 1000:
-    powers = 1
-    for i in range(1, k + 1):
-        #
-        if 10**powers < i:
-            powers += 1
-        # sum of narcissistic numbers
-        narcissisticNumbSum = 0
-        # a string showing the sum of the narcissistic numbers
-        narcissisticNumbStr = ""
-        # whole part of division
-        divWholePart = i
 
-        while divWholePart > 0:
-            # get remainder of division
-            # last digit of numbers
-            divRemainder = divWholePart % 10
+powers = 1
+for i in range(1001):
+    # exit from the loop, to exclude unnecessary work of the loop
+    if i > k:
+        break
+    if 10**powers < i:
+        powers += 1
+    # sum of narcissistic numbers
+    narcissisticNumbSum = 0
+    # a string showing the sum of the narcissistic numbers
+    printList = []
+    # whole part of division
+    divWholePart = i
 
-            narcissisticNumbSum += divRemainder ** powers
+    while divWholePart > 0:
+        # get remainder of division
+        # last digit of numbers
+        divRemainder = divWholePart % 10
 
-            # conversion for rendering information
-            if narcissisticNumbStr:
-                narcissisticNumbStr = " + " + narcissisticNumbStr
-            narcissisticNumbStr = str(divRemainder) + "**" + str(powers) + narcissisticNumbStr
+        narcissisticNumbSum += divRemainder ** powers
 
-            # get the new integer part of the narcissistic number
-            divWholePart //= 10
+        # conversion for rendering information
+        listOfDivRemainderInPower = [divRemainder, "^", powers]
 
-        if i == narcissisticNumbSum:
-            print("Narcissistic number: ", i, " = ", narcissisticNumbStr)
-else:
-    # if the user entered an incorrect k, show auxiliary information
-    print("You entered wrong k, k must be 1 <= k <= 1000")
+        # get the new integer part of the narcissistic number
+        divWholePart //= 10
+
+        if len(printList) > 0:
+            listOfDivRemainderInPower.append(" + ")
+            listOfDivRemainderInPower.extend(printList)
+
+        printList = listOfDivRemainderInPower
+
+    if i and i == narcissisticNumbSum:
+        print("Narcissistic number: ", i, " = ", end='')
+        for j in printList:
+            print(j, end='')
+        print("")
