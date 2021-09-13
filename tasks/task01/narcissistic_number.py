0510502002ecdc82e5753bf53aba8312484a30ef
myepishkin <myepishkin@exadel.com>
2021-09-13 14:05:01 +0300

    task01. Narcissistic numbers. Code cleanup.

--- tasks/task01/narcissistic_number.py
+++ tasks/task01/narcissistic_number.py
@@ -1,40 +1,27 @@
 k = int(input('Please enter an integer k(1<=k<=1000): '))
 
 powers = 1
-for i in range(1001):
-    # exit from the loop, to exclude unnecessary work of the loop
-    if i > k:
-        break
-    if 10**powers < i:
-        powers += 1
+for i in range(k+1):
     # sum of narcissistic numbers
     narcissisticNumbSum = 0
     # a string showing the sum of the narcissistic numbers
-    printList = []
+    printString = ""
     # whole part of division
     divWholePart = i
+
+    if 10**powers < i:
+        powers += 1
 
     while divWholePart > 0:
         # get remainder of division
         # last digit of numbers
         divRemainder = divWholePart % 10
-
         narcissisticNumbSum += divRemainder ** powers
-
         # conversion for rendering information
-        listOfDivRemainderInPower = [divRemainder, "^", powers]
-
+        stringDivRemainderInPower = f"{divRemainder}^{powers}"
+        printString = f"{stringDivRemainderInPower} + {printString}" if printString else stringDivRemainderInPower
         # get the new integer part of the narcissistic number
         divWholePart //= 10
 
-        if len(printList) > 0:
-            listOfDivRemainderInPower.append(" + ")
-            listOfDivRemainderInPower.extend(printList)
-
-        printList = listOfDivRemainderInPower
-
     if i and i == narcissisticNumbSum:
-        print("Narcissistic number: ", i, " = ", end='')
-        for j in printList:
-            print(j, end='')
-        print("")
+        print(f"Narcissistic number: {i} = {printString}")
